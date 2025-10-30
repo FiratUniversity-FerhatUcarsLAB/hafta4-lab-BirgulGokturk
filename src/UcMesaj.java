28b578c1a11395e221e131317bdd44d4dd7c0578
BirgulGokturk <250541094@firat.edu.tr>
2025-10-30 05:35:38 +0300

    Delete src directory

--- src/UcMesaj.java
+++
@@ -1,7 +0,0 @@
-public class UcMesaj {
-    public static void main (String[] args) {
-        System.out.println("Java'ya Hos Geldiniz");
-        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
-        System.out.println("Programlama Eğlencelidir");
-    }
-}
